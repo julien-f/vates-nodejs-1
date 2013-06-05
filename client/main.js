16c85de8aead8d28e7749b17b1f41d4ae20d3066
Chris Allard <lyon.allard.chris@gmail.com>
2013-06-05 14:37:46 +0200

    --add-host and --rm--hos added

--- client/main.js
+++ client/main.js
@@ -1,25 +1,29 @@
 #!/usr/bin/nodejs
 
-var http = require('http');
+var http = require('http'); //Variable de type HTTP
 
-var json_rpc_id = 0;
-function json_call(method, params, callback)
+var json_rpc_id = 0; // Variable pour l'ID des requètes en JSONRPC
+
+function json_call(method, params, callback) //Fonction qui passe la méthode et le paramètre en JSON
 {
-	// On créé une requête HTTP de type POST.
-	http.request(
-		{
-			'hostname': 'localhost',
-			'port':     8080,
-			'method':   'POST',
-		},
-		function(resp) {
-			// Nous avons reçu un début de réponse.
 
-			resp.bind('data', function(data) {
+var option = 
+	{
+		hostname: 'localhost',
+		port: '8080',
+		path: '/',
+		method: 'POST'
+	};
+	
+	// On créé une requête HTTP de type POST.
+var req = http.request(option,function(resp) 
+		{
+			// Nous avons reçu un début de réponse.
+			resp.on('data', function(data) {
 				// Nous avons reçu le corps de la réponse, on le parse
 				// et appelle la fonction callback() avec son
 				// résultat.
-				callback(JSON.parse(data).result);
+				callback(JSON.parse(data.toString()).result);//Fonction callback avec le resultat de la réponse JSONRPC du serveur
 			});
 		}
 	).end( // On envoie notre requête avec la requête JSON-RPC.
@@ -36,21 +40,36 @@ function json_call(method, params, callback)
 
 function add_host(label)
 {
-	json_call('add_host', [label], function() {
-		console.log('The host has been added.');
+json_call('--add-host', [label], function(result) {
+		if (result)
+		{
+			console.log('The host has been added.');
+		}
+		else
+		{
+			console.log('Error, the host name already exist');
+		}
 	});
 }
 
 function rm_host(label)
 {
-	json_call('rm_host', [label], function() {
-		console.log('The host has been removed.');
+	json_call('--rm-host', [label], function(result) {
+		if (result)
+		{
+			console.log('The host has been deleted.');
+		}
+		else
+		{
+			console.log('Error, the host name doesn\'y exist');
+		}
 	});
 }
 
 function listen_host(label)
 {
-
+	json_call('--listen-host', [label], function(result){
+	
 }
 
 function listen_all_hosts()
@@ -58,7 +77,7 @@ function listen_all_hosts()
 
 }
 
-//////////////////////////////////////////////////////////////////////
+///////////////////////////////////////
 
 var args = process.argv.slice(2);
 
@@ -71,5 +90,32 @@ if (!args.length)
 // --add-host <host> --rm-host <host> --listen-host <host> --listen-all-hosts
 for (var i = 0, n = args.length; i < n; ++i)
 {
-	// todo
+	if (args[i]=='--add-host') // If the argument is --add-host
+	{
+		console.log('command : ' +args[i] + ' argument : ' +args[i+1]);
+		add_host(args[i+1]);
+		i++;
+	}
+	else if (args[i]=='--rm-host')// If the argument is --rm-host
+	{
+		console.log('command : ' +args[i] + ' argument : ' +args[i+1]);
+		rm_host(args[i+1]);
+		i++;
+	}
+	else if(args[i]=='--listen-host')// If the argument is --listen-host
+	{
+		console.log('command : ' +args[i] + ' argument : ' +args[i+1]);
+		listen_host(args[i+1]);
+		i++;
+	}
+	else if(args[i]=='--listen-all-hosts')// If the argument is --listen-all-hosts
+	{
+		console.log('command : ' +args[i] + ' argument : ' +args[i+1]);
+		listen_all_hosts(args[i+1]);
+		i++;
+	}
+	else // If one of all arguments doesn't existe
+	{
+		console.log('Unknown command in argurment : ' + args[i]);
+	}
 }
--- server/main.js
+++ server/main.js
@@ -1,1 +1,89 @@
 #!/usr/bin/nodejs
+var http = require('http');
+var EventEmitter = require('events').EventEmitter;
+
+
+Array.prototype.unset = function(val){
+	var index = this.indexOf(val)
+	if(index > -1){
+		this.splice(index,1)
+	}
+}
+
+var host = new Array();
+var signal = new EventEmitter();
+
+///////////////////////////////////////
+server = http.createServer(function(req, res)
+{
+	//On recupère la requete du client qui contient des informations en JSONRPC
+	req.on('data', function(data) {
+	//On partitionne l'information envoyer après avoir transformer en string le buffer reçu
+	data = JSON.parse(data.toString());
+	
+		if (data.method=='--add-host') //Si dans le Data reçu, la méthode envoyé est --add-host
+		{
+			if (host.indexOf(data.params[0])===-1) // Si l'host n'existe pas dans notre tableau
+			{
+				host.push(data.params[0]); //On ajoute l'host dans le tableau des host
+				console.log('Host : ' + data.params[0] + ' has been added');
+				
+				signal.emit('--add-host-', data.params[0]);
+					
+				res.end( // On retourne une réponse positive au client
+					JSON.stringify({
+						'jsonrpc': '2.0',
+						'result': true,
+						'id': data.id,
+					})
+				);
+			}
+			else // Si l'host existe déja dans notre tableau
+			{
+				res.end( // On retourne une réponse négative au client
+					JSON.stringify({
+						'jsonrpc': '2.0',
+						'result': false,
+						'id': data.id,
+					})
+				);
+			}
+		}
+
+		else if(data.method=='--rm-host') //Si dans le Data reçu, la méthode envoyé est --rm-host
+		{
+			if (host.indexOf(data.params[0])===-1) // Si l'host n'existe pas dans notre tableau
+			{
+				res.end( // On retourne une réponse négative au client
+					JSON.stringify({
+						'jsonrpc': '2.0',
+						'result': false,
+						'id': data.id,
+					})
+				);
+			}
+			else // Si l'host existe déja dans notre tableau
+			{
+				host.unset(data.params[0]);
+				console.log('Host :' + data.params[0] + ' has been deleted');
+				res.end( // On retourne une réponse positive au client
+					JSON.stringify({
+						'jsonrpc': '2.0',
+						'result': true,
+						'id': data.id,
+					})
+				);
+			}
+		}
+		
+		else if(data.method=='--listen-host')
+		{
+		
+		
+		
+	});
+});
+
+server.listen(8080);
+
+
